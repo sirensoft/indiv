7522ebc1f6704508b5a9ac75e9932df899ae10b2
tehnn <tehnnn@gmail.com>
2017-02-15 09:31:05 +0700

    utehn-smartcare

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -1,5 +1,4 @@
 <?php
-
 /* @var $this \yii\web\View */
 /* @var $content string */
 
@@ -15,73 +14,89 @@ AppAsset::register($this);
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
-<head>
-    <meta charset="<?= Yii::$app->charset ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?= Html::csrfMetaTags() ?>
-    <title><?= Html::encode($this->title) ?></title>
-    <?php $this->head() ?>
-</head>
-<body>
-<?php $this->beginBody() ?>
+    <head>
+        <meta charset="<?= Yii::$app->charset ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <?= Html::csrfMetaTags() ?>
+        <title><?= Html::encode($this->title) ?></title>
+        <?php $this->head() ?>
+    </head>
+    <body>
+        <?php $this->beginBody() ?>
 
-<div class="wrap">
-    <?php
-    NavBar::begin([
-        'brandLabel' => '<span class="glyphicon glyphicon-list-alt"></span>',
-       'brandUrl' => Yii::$app->homeUrl,
-        'options' => [
-            'class' => 'navbar-custom navbar-fixed-top',
-        ],
-    ]);
-     echo Nav::widget([
+        <div class="wrap">
+            <?php
+            NavBar::begin([
+                'brandLabel' => '<span class="glyphicon glyphicon-list-alt"></span>',
+                'brandUrl' => Yii::$app->homeUrl,
+                'options' => [
+                    'class' => 'navbar-custom navbar-fixed-top',
+                ],
+            ]);
+            echo Nav::widget([
                 'options' => ['class' => 'navbar-nav navbar-left'],
                 'encodeLabels' => false,
                 'items' => [['label' => 'HDC-INDIV']],
             ]);
-    $menuItems = [
-        //['label' => 'Home', 'url' => ['/site/index']],
-        ['label' => 'เกี่ยวกับ', 'url' => ['/site/about']],
+            $menuItems = [
+                //['label' => 'Home', 'url' => ['/site/index']],
+                ['label' => 'เกี่ยวกับ', 'url' => ['/site/about']],
+            ];
+            if (Yii::$app->user->isGuest) {
+                //$menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
+                //$menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
+            } else {
+                $menuItems[] = '<li>'
+                        . Html::beginForm(['/site/logout'], 'post')
+                        . Html::submitButton(
+                                'Logout (' . Yii::$app->user->identity->username . ')', ['class' => 'btn btn-link logout']
+                        )
+                        . Html::endForm()
+                        . '</li>';
+            }
+            echo Nav::widget([
+                'options' => ['class' => 'navbar-nav navbar-right'],
+                'items' => $menuItems,
+            ]);
+            NavBar::end();
+            ?>
+
+            <div class="container">
+            <?=
+            Breadcrumbs::widget([
+                'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+            ])
+            ?>
+                <?= Alert::widget() ?>
+                <?= $content ?>
+            </div>
+        </div>
+      
+          
         
-    ];
-    if (Yii::$app->user->isGuest) {
-        //$menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
-        //$menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
-    } else {
-        $menuItems[] = '<li>'
-            . Html::beginForm(['/site/logout'], 'post')
-            . Html::submitButton(
-                'Logout (' . Yii::$app->user->identity->username . ')',
-                ['class' => 'btn btn-link logout']
-            )
-            . Html::endForm()
-            . '</li>';
-    }
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav navbar-right'],
-        'items' => $menuItems,
-    ]);
-    NavBar::end();
-    ?>
+        <footer class="footer">
+            <div class="container">
+                
+                <p class="pull-left" id="histats_counter"></p>
 
-    <div class="container">
-        <?= Breadcrumbs::widget([
-            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-        ]) ?>
-        <?= Alert::widget() ?>
-        <?= $content ?>
-    </div>
-</div>
+                <p class="pull-right">&copy; นายอุเทน จาดยางโทน</p>
+            </div>
+        </footer>
 
-<footer class="footer">
-    <div class="container">
-        <p class="pull-left"><?= date('Y') ?></p>
-
-        <p class="pull-right">&copy; นายอุเทน จาดยางโทน</p>
-    </div>
-</footer>
-
-<?php $this->endBody() ?>
-</body>
+        <?php $this->endBody() ?>
+    </body>
 </html>
 <?php $this->endPage() ?>
+<script type="text/javascript">
+     var _Hasync = _Hasync || [];
+                _Hasync.push(['Histats.start', '1,3735892,4,2004,112,35,00011001']);
+                _Hasync.push(['Histats.fasi', '1']);
+                _Hasync.push(['Histats.track_hits', '']);
+                (function () {
+                    var hs = document.createElement('script');
+                    hs.type = 'text/javascript';
+                    hs.async = true;
+                    hs.src = ('//s10.histats.com/js15_as.js');
+                    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+                })();    
+</script>
